e6f8730719a696997bb648c3346bcd2f9ab75684
SIVA <sivagurukarthickeyan@gmail.com>
2024-04-11 11:59:53 +0530

    COMPLETING BACKEND FUNCTIONALITY AND UPDATING ACCORINGLY WITH CLIENT

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import Spinner from "../components/Spinner";
 import Repos from "../components/Repos";
 
 const ExplorePage = () => {
+  // https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=10
   const [loading, setLoading] = useState(false);
   const [repos, setRepos] = useState([]);
-  const [selectedLanguage, setSelectedLanguage] = useState('');
-
+  const [selectedLanguage, setSelectedLanguage] = useState("");
 
   const exploreRepos = async (language) => {
     setLoading(true);
     setRepos([]);
     try {
-      const res = await fetch("http://localhost:5000/api/explore/repos/" + language);
-      const {repos}= await res.json();
+      const res = await fetch("/api/explore/repos/" + language);
+      const { repos } = await res.json();
       setRepos(repos);
+
       setSelectedLanguage(language);
     } catch (error) {
       toast.error(error.message);
     } finally {
       setLoading(false);
     }
-   };
+  };
   return (
     <div className="px-4">
       <div className="bg-glass max-w-2xl mx-auto rounded-md p-4">
@@ -32,7 +33,7 @@ const ExplorePage = () => {
         <div className="flex flex-wrap gap-2 my-2 justify-center">
           <img
             src="/javascript.svg"
-            alt="JavaScript"
+            alt="JavaScript ogo"
             className="h-11 sm:h-20 cursor-pointer"
             onClick={() => exploreRepos("javascript")}
           />
@@ -77,5 +78,4 @@ const ExplorePage = () => {
     </div>
   );
 };
-
 export default ExplorePage;
